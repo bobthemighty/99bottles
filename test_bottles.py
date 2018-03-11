536916e5fa26eb3ec2f88c6fb3e0a888dd94828d
Bob Gregory <bob@made.com>
2018-03-11 21:20:04 +0000

    Three tests passing

--- test_bottles.py
+++ test_bottles.py
@@ -5,13 +5,18 @@ def clean(s):
 
 class BottleSong:
 
+    def describe_bottles(self, num):
+        if num == 1:
+            return "1 bottle"
+        return f"{num} bottles"
+
     def verse(self, num):
         return clean(f"""
-        {num} bottles of beer on the wall
-        {num} bottles of beer
+        {self.describe_bottles(num)} of beer on the wall
+        {self.describe_bottles(num)} of beer
         Take one down
         Pass it around
-        {num - 1} bottles of beer on the wall
+        {self.describe_bottles(num - 1)} of beer on the wall
         """)
 
 
@@ -35,3 +40,16 @@ def test_another_verse():
         Pass it around
         2 bottles of beer on the wall
     """)
+
+
+def test_next_to_penultimate_verse():
+    song = BottleSong()
+    assert song.verse(2) == clean("""
+        2 bottles of beer on the wall
+        2 bottles of beer
+        Take one down
+        Pass it around
+        1 bottle of beer on the wall
+    """)
+
+
